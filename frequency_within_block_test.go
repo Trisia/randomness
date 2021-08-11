7d3282e018e137c0a768ac967de65320bbd8fe3b
cliven <quanguanyu@qq.com>
2021-08-11 19:03:25 +0800

    增加了数据生成

--- frequency_within_block_test.go
+++ frequency_within_block_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestFrequencyWithinBlockTest(t *testing.T) {
 	bits := GroupBit()
+	//bits := ReadGroup("data.bin")
 	p := FrequencyWithinBlockTest(bits)
 	fmt.Printf("n: 1000000, P-valye: %f\n", p)
 }
--- mono_bit_frequency_test.go
+++ mono_bit_frequency_test.go
@@ -2,21 +2,16 @@ package randomness
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"os"
 	"testing"
 	"time"
 )
 
-// GroupBit 生成一组测试数据 长度为 10^6 比特
 func GroupBit() []bool {
 	n := 1000_000
 	bits := make([]bool, 0, n)
-	//buf := make([]byte, n/8)
-	//_, _ = rand.Read(buf)
-	//for _, b := range buf {
-	//	bits = append(bits, B2bit(b)...)
-	//}
-	//return bits
 	rand.Seed(time.Now().Unix())
 	for i := 0; i < n; i++ {
 		if rand.Int()%2 == 1 {
@@ -28,8 +23,46 @@ func GroupBit() []bool {
 	return bits
 }
 
+// GroupBit 生成一组测试数据 长度为 10^6 比特
+func GroupSecBit() []bool {
+	n := 1000_000
+	bits := make([]bool, 0, n)
+
+	buf := make([]byte, n/8)
+	_, _ = rand.Read(buf)
+	for _, b := range buf {
+		bits = append(bits, B2bit(b)...)
+	}
+	return bits
+}
+
+func ReadGroup(filename string) []bool {
+	n := 1000_000
+	bits := make([]bool, 0, n)
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	for _, b := range buf {
+		bits = append(bits, B2bit(b)...)
+	}
+	return bits
+}
+
+func TestGroupBit(t *testing.T) {
+	n := 1000_000
+	buf := make([]byte, n/8)
+
+	_, _ = rand.Read(buf)
+	err := ioutil.WriteFile("data.bin", buf, os.ModePerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestMonoBitFrequencyTest(t *testing.T) {
 	bits := GroupBit()
+	//bits := ReadGroup("data.bin")
 	p := MonoBitFrequencyTest(bits)
-	fmt.Printf("n: 1000000, P-valye: %0.2f\n", p)
+	fmt.Printf("n: 1000000, P-value: %.6f\n", p)
 }
