ea3fb3774f2e7a5314b7d30f2fc5cf31e9e96757
Sun Yimin <emmansun@users.noreply.github.com>
2022-10-28 10:16:00 +0800

    fix期待结果

--- maurers_universal_test.go
+++ maurers_universal_test.go
@@ -9,7 +9,7 @@ func TestMaurerUniversalTestSample(t *testing.T) {
 	bits := ReadGroupInASCIIFormat("data/data.e")
 	p, q := MaurerUniversalTest(bits)
 	fmt.Printf("n: %v, P-value: %f, Q-value: %f\n", len(bits), p, q)
-	if fmt.Sprintf("%.6f", p) != "0.282568" || fmt.Sprintf("%.6f", q) != "0.282568" {
+	if fmt.Sprintf("%.6f", p) != "0.282568" || fmt.Sprintf("%.6f", q) != "0.141284" {
 		t.FailNow()
 	}
 }
